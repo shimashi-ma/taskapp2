8734c4188c6f0513dfc887fddf92138509f5b61e
mikako.kinugawa <kinugawa0807@yahoo.co.jp>
2019-10-16 15:40:14 +0900

    Initial Commit

---
+++ taskapp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp2
+//
+//  Created by mikako kinugawa on 2019/10/16.
+//  Copyright © 2019 mikako.kinugawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
